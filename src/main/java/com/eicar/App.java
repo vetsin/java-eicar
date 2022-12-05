4d94270ae8f66b307468a80f56f72a8fec56d858
matthew.gill <matthew.gill@servicenow.com>
2022-12-05 13:50:11 -0800

    initial

---
+++ src/main/java/com/eicar/App.java
@@ -0,0 +1,13 @@
+package com.eicar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" );
+    }
+}
